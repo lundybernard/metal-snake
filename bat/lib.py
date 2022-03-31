c140526545aa49dc2fc435aa8c029ee7d13b1093
LundyBernard <lundy.bernard@gmail.com>
2022-03-31 15:38:57 -0500

    add distance problem numpy solution

--- bat/lib.py
+++ bat/lib.py
@@ -1,3 +1,47 @@
+from typing import List, Dict
+
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+def minimize_distance(features: List[Dict[str, bool]]) -> int:
+    # convert features to list of Binary Lists
+    bin_matrix = [
+        [d[k] for d in features]
+        for k, v in features[0].items()
+    ]
+
+    # perform a distance-transform on each list
+    # ex: [1, 0, 0, 0, 1] => [0, 1, 2, 1, 0]
+    distmap = np.array([distance_transform(L) for L in bin_matrix])
+    '''
+    [[0 1 1 0 1 2]
+     [3 2 1 0 1 0]
+     [0 0 1 1 0 1]]
+    '''
+    print(f'{distmap=}')
+    print(f'{distmap.T=}')
+    ret = distmap.T.sum(axis=1)
+    print(f'{ret=}')
+    ret = ret.argmin()
+    print(f'{ret=}')
+    return ret
+
+
+
+def distance_transform(A: List[bool]) -> List[int]:
+    A = np.array(A)
+    print(f'{A=}')
+    a = np.argwhere(A)
+    na = np.argwhere(~A)
+
+    m = np.zeros(A.shape, dtype=int)
+    print(f'{m=}')
+    m[tuple(na.T)] = cdist(a, na, 'cityblock').min(0, initial=len(A))
+    print(f'{m=}')
+
+    return m
+
 
 def hello_world():
     return 'Hello World!'
--- bat/tests/lib_test.py
+++ bat/tests/lib_test.py
@@ -1,9 +1,57 @@
 from unittest import TestCase
 
-from bat.lib import hello_world
+from bat.lib import (
+    minimize_distance,
+    distance_transform,
+    hello_world,
+    np,
+)
 
 
 class LibTests(TestCase):
+
+    def test_minimize_distance(t):
+        # Given a list of dicts representing features on nodes
+        # find the node (list index)
+        # which minimizes the distance to all features
+        with t.subTest('simple'):
+            features = [
+                {'grocery': True, 'park': False, 'bar': True},
+                {'grocery': False, 'park': False, 'bar': True},
+                {'grocery': False, 'park': False, 'bar': False},
+                {'grocery': True, 'park': True, 'bar': False},
+                {'grocery': False, 'park': False, 'bar': True},
+                {'grocery': False, 'park': True, 'bar': False},
+            ]
+            ret = minimize_distance(features)
+            t.assertEqual(ret, 3)
+
+        with t.subTest('multiple solutions'):
+            # Returns the lowest index (first) solution
+            features = [
+                {'grocery': False, 'park': False, 'bar': False},
+                {'grocery': True, 'park': True, 'bar': True},
+                {'grocery': True, 'park': True, 'bar': True},
+            ]
+            ret = minimize_distance(features)
+            t.assertEqual(ret, 1)
+
+        with t.subTest('no solution'):
+            # returns the best match
+            features = [
+                {'grocery': False, 'park': False, 'bar': False},
+                {'grocery': False, 'park': False, 'bar': False},
+                {'grocery': False, 'park': True, 'bar': True},
+            ]
+            ret = minimize_distance(features)
+            t.assertEqual(ret, 2)
+
+    def test_distance_transform(t):
+        # Given a List of Boolian values
+        # return an array of integers for the distance to each True value
+        data = [True, False, False, False, True, False]
+        ret = distance_transform(data)
+        np.testing.assert_array_equal(ret, np.array([0, 1, 2, 1, 0, 1]))
 
     def test_hello_world(t):
         ret = hello_world()
